a51f76dd1fef6e788d5cd68f63f0f4bff8e16998
tlras <14915607+tlras@users.noreply.github.com>
2023-07-07 20:35:03 -0500

    lol

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Popcat from "./popcat.svelte";
+</script>
+
+<Popcat />
+<Popcat />
+<Popcat />
+<Popcat />
---
+++ src/routes/popcat.svelte
@@ -0,0 +1,26 @@
+<script>
+    let audio;
+
+    const CLOSED = "https://slaska.xyz/closed.png";
+    const OPEN = "https://slaska.xyz/open.png";
+    let cat_src = CLOSED;
+
+    function pop() {
+        cat_src = OPEN;
+        audio.currentTime = 0;
+        audio.play();
+    }
+
+    function unpop() {
+        cat_src = CLOSED;
+    }
+</script>
+
+<audio src="https://slaska.xyz/pop.ogg" bind:this={audio}></audio>
+<img
+    src={cat_src}
+    alt="pop cat"
+    width="100px"
+    on:pointerdown={pop}
+    on:pointerup={unpop}
+>
